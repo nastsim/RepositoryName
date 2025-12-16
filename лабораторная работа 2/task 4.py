0bdfdbd21b48e979e4a807bcbf3f2f3caa794c62
nastsim <lagunov14@mail.ru>
2025-12-16 22:39:23 +0500

    Add files via upload

---
+++ лабораторная работа 2/task 4.py
@@ -0,0 +1,8 @@
+list_digit = [4, 9, 5, 1, 8, 4]
+print(list_digit)
+
+print("Сумма цифр", sum(list_digit))
+print("Количество цифр", len(list_digit))
+print("Минимальная цифра", min(list_digit))
+print("Максимальная цифра", max(list_digit))
+print("Среднее арифметическое цифр", round(sum(list_digit) / len(list_digit), 2))
---
+++ лабораторная работа 2/task 5.py
@@ -0,0 +1,7 @@
+str_ = "sdkfjsdkjfnksjdnfkjsdnfkjsndfkjsndfkjsndfkjsnd"
+
+# Находим уникальные символы в строке с помощью множества
+unique_chars = set(str_)
+
+# Выводим количество уникальных символов в строке
+print(len(unique_chars))
