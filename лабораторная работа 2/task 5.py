ff2e7d9a781a9097e824aaed56d31ea3224834e3
nastsim <lagunov14@mail.ru>
2026-01-21 21:40:53 +0500

    task 5.py

--- лабораторная работа 2/task 5.py
+++ лабораторная работа 2/task 5.py
@@ -3,5 +3,5 @@ str_ = "sdkfjsdkjfnksjdnfkjsdnfkjsndfkjsndfkjsndfkjsnd"
 # Находим уникальные символы в строке с помощью множества
 unique_chars = set(str_)
 
-# Выводим количество уникальных символов в строке
+# Выводим кол-во уникальных символов в строке
 print(len(unique_chars))
