7f9d41b5b797475169cc15766bd787ec3a2381fc
nastsim <lagunov14@mail.ru>
2026-01-21 22:04:47 +0500

    task 4.py

--- лабораторная работа 2/task 4.py
+++ лабораторная работа 2/task 4.py
@@ -2,7 +2,7 @@ list_digit = [4, 9, 5, 1, 8, 4]
 print(list_digit)
 
 print("Сумма цифр", sum(list_digit))
-print("Кол-во цифр", len(list_digit))
+print("Количество цифр", len(list_digit))
 print("Минимальная цифра", min(list_digit))
 print("Максимальная цифра", max(list_digit))
 print("Среднее арифметическое цифр", round(sum(list_digit) / len(list_digit), 2))
